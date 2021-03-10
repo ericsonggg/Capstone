b15a35fce41d48f7262d01783094e31c99dd3a90
Eric Song <ericsongg@hotmail.com>
2021-03-10 10:48:17 -0500

    Removed unused settings

--- app/src/main/java/com/example/coolerthanyou/ui/MainActivity.kt
+++ app/src/main/java/com/example/coolerthanyou/ui/MainActivity.kt
@@ -99,12 +99,6 @@ class MainActivity : BaseActivity() {
         logger.d(logTag, "onCreate completed")
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.main, menu)
-        return true
-    }
-
     override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(R.id.activity_main_fragment_host)
         return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
